1a53418f16d37dc06e14397755cf844a167c8aab
iDraKzy <youlan.collard.student@elmarche.be>
2019-11-13 00:44:12 +0100

    fix: RegEx italic

--- commands/tvshow/showinfo.js
+++ commands/tvshow/showinfo.js
@@ -43,7 +43,7 @@ module.exports = class ShowInfoCommand extends Command {
                 }
                 const showEmbed = new RichEmbed()
                     .setTitle(`Informations à propos de la série ${res.displayName}`)
-                    .setDescription(`Synopsis en anglais : \n\n${res.synopsis.replace(/<br>/g, "\n").replace(/<(\/)?b>/g, "**").replace(/<(\/)?i>/, "*")}`) //TODO: Translation
+                    .setDescription(`Synopsis en anglais : \n\n${res.synopsis.replace(/<br>/g, "\n").replace(/<(\/)?b>/g, "**").replace(/<(\/)?i>/g, "*")}`) //TODO: Translation
                     .setImage(res.image.slice(0, -3) + "jpg")
                     .setColor("#8E44AD")
                     .addField("Statut", res.status) //TODO: Translation
